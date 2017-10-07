51ca77d1c81b6d74d8fdf29562d98e622d5d591c
William Gillespie <wgilles1@emich.edu>
2017-10-07 15:58:19 -0400

    Update of the steering histogram creation file.

--- steering_histogram_one_file.py
+++ steering_histogram_one_file.py
@@ -1,11 +1,16 @@
-
+import seaborn as sns
 import matplotlib.pyplot as plt
 
-with open('MyData4/driving_log.csv') as f:
+sns.set_style("whitegrid")
+filename = '../ledata/MyData9/driving_log.csv'  # change the filenate to your desired file
+with open(filename) as f:
     steering_list = []
     f.readline()
     for line in f:
         comps = line.split(",")
         steering_list.append(float(comps[3]))
-    plt.hist(steering_list, bins=50)
+    plt.hist(steering_list, bins=20)
+    plt.xlabel('Steering value\n(negative -> turning left; positive -> turning right)', fontsize=16)
+    plt.ylabel('count of steering value', fontsize=16)
+    plt.title('Histogram of steering values in sample file', fontsize=20)
     plt.show()
