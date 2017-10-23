bfd0df341c9118af03ff6c4ed1dd62196d482332
William Gillespie <wgilles1@emich.edu>
2017-10-22 21:20:27 -0400

    Modification to drive.py because the model was trained with BGR format.

--- drive.py
+++ drive.py
@@ -60,6 +60,8 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
+        r, g, b = image.split()
+        image = Image.merge("RGB", (b, g, r))
         image_array = np.asarray(image)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
